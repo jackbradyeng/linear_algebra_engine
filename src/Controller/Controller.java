77446715cdd00bed14b6ac27991e35e60b5f1992
jackb <jackbradyx@proton.me>
2025-11-25 20:07:30 +1000

    Refactored controller class. - Removed print method calls for matrix operations actionPerformed() blocks. - Nested actionPerformed() blocks into their own private methods. - Set controller helper methods to private.

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -13,6 +13,7 @@ import java.awt.event.ActionListener;
  */
 public class Controller implements ActionListener, ChangeListener {
 
+    //private constants
     private static final int MATRIX_GRID_UPPER_LIMIT = 10;
     private static final int MATRIX_GRID_LOWER_LIMIT = 1;
 
@@ -44,110 +45,40 @@ public class Controller implements ActionListener, ChangeListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == view.add) {
-            model.setMatrixThree(model.getMatrixOne().add(model.getMatrixOne(), model.getMatrixTwo()));
-            view.renderProductMatrix(model.getMatrixThree());
-            model.getMatrixThree().printMatrix();
+            addMatrices();
         }
         else if (e.getSource() == view.multiply) {
-            model.setMatrixThree(model.getMatrixOne().multiply(model.getMatrixOne(), model.getMatrixTwo()));
-            view.renderProductMatrix(model.getMatrixThree());
-            model.getMatrixThree().printMatrix();
+            multiplyMatrices();
         }
         else if (e.getSource() == view.downsize) {
-            if(model.getMatrixSize() <= MATRIX_GRID_LOWER_LIMIT) {
-                System.out.println("Matrices cannot be decremented any further.");
-            } else {
-                model.setMatrixSize(model.getMatrixSize() - 1);
-                resetMatrixOne();
-                resetMatrixTwo();
-                resetIdentityMatrix();
-                resetDetLabels();
-                view.clearProductMatrix(); //if we change the size of our matrices then any pre-existing data in the
-                //product matrix should be cleared
-                System.out.println("Matrices decremented.");
-            }
+            downsizeMatrices();
         }
         else if (e.getSource() == view.upsize) {
-            if(model.getMatrixSize() >= MATRIX_GRID_UPPER_LIMIT) {
-                System.out.println("Matrices cannot be incremented any further.");
-            } else {
-                model.setMatrixSize(model.getMatrixSize() + 1);
-                resetMatrixOne();
-                resetMatrixTwo();
-                resetIdentityMatrix();
-                resetDetLabels();
-                view.clearProductMatrix();
-                System.out.println("Matrices incremented.");
-            }
+            upsizeMatrices();
         }
         else if(e.getSource() == view.generateA) {
-            //call the generator's relevant method to return a new matrix
-            model.setMatrixOne(new Matrix(model.generator.generateRandomMatrix(model.getMatrixSize(),
-                    model.getMatrixSize())));
-            //update the identity matrix in our model
-            model.setIdentityMatrix(new Matrix(model.getMatrixOne().getIdentity()));
-            //clear any pre-existing data in the product matrix and set the instance to null
-            model.setMatrixThree(null);
-            view.clearProductMatrix();
-            view.renderMatrixOne(model.getMatrixOne());
-            view.detALabel.setText("Determinant: ");
-            model.getMatrixOne().printMatrix();
+            generateA();
         }
         else if(e.getSource() == view.generateB) {
-            model.setMatrixTwo(new Matrix(model.generator.generateRandomMatrix(model.getMatrixSize(),
-                    model.getMatrixSize())));
-            model.setIdentityMatrix(new Matrix(model.getMatrixTwo().getIdentity()));
-            model.setMatrixThree(null);
-            view.clearProductMatrix();
-            view.renderMatrixTwo(model.getMatrixTwo());
-            view.detBLabel.setText("Determinant: ");
-            model.getMatrixTwo().printMatrix();
+            generateB();
         }
         else if(e.getSource() == view.identityA) {
-            if(!model.getMatrixOne().isIdentityInUse()) {
-                view.renderMatrixOne(model.getIdentityMatrix());
-                model.getMatrixOne().setIdentityInUse(true);
-                //after this, if the Matrix A getter method is called, the identity matrix will be returned
-            } else {
-                model.getMatrixOne().setIdentityInUse(false);
-                view.renderMatrixOne(model.getMatrixOne());
-                //after this, the Matrix A getter method will go back to returning the original matrix
-            }
+            viewIdentityA();
         }
         else if(e.getSource() == view.identityB) {
-            if(!model.getMatrixTwo().isIdentityInUse()) {
-                view.renderMatrixTwo(model.getIdentityMatrix());
-                model.getMatrixTwo().setIdentityInUse(true);
-            } else {
-                model.getMatrixTwo().setIdentityInUse(false);
-                view.renderMatrixTwo(model.getMatrixTwo());
-            }
+            viewIdentityB();
         }
         else if (e.getSource() == view.scaleA) {
-            model.setMatrixOne(model.getMatrixOne().scale(model.getMatrixOne(), model.getScaleFactorOne()));
-            view.renderMatrixOne(model.getMatrixOne());
-            model.setMatrixThree(null);
-            view.clearProductMatrix();
-            view.detALabel.setText("Determinant: ");
-            System.out.println("Matrix A scaled by a factor of: " + model.getScaleFactorOne());
+            scaleA();
         }
         else if (e.getSource() == view.scaleB) {
-            model.setMatrixTwo(model.getMatrixTwo().scale(model.getMatrixTwo(), model.getScaleFactorTwo()));
-            view.renderMatrixTwo(model.getMatrixTwo());
-            model.setMatrixThree(null);
-            view.clearProductMatrix();
-            view.detBLabel.setText("Determinant: ");
-            System.out.println("Matrix B scaled by a factor of: " + model.getScaleFactorTwo());
+            scaleB();
         }
         else if (e.getSource() == view.determinantA) {
-            model.getMatrixOne().setDeterminant(model.getMatrixOne());
-            view.detALabel.setText("Determinant: " + model.getMatrixOne().getDeterminant());
-            System.out.println("Matrix determinant is: " + model.getMatrixOne().getDeterminant());
+            viewDeterminantA();
         }
         else if (e.getSource() == view.determinantB) {
-            model.getMatrixTwo().setDeterminant(model.getMatrixTwo());
-            view.detBLabel.setText("Determinant: " + model.getMatrixTwo().getDeterminant());
-            System.out.println("Matrix determinant is: " + model.getMatrixTwo().getDeterminant());
+            viewDeterminantB();
         }
     }
 
@@ -165,24 +96,138 @@ public class Controller implements ActionListener, ChangeListener {
         }
     }
 
-    public void resetMatrixOne() {
+    private void resetMatrixOne() {
         model.setMatrixOne(new Matrix(model.generator.generateRandomMatrix(model.getMatrixSize(),
                 model.getMatrixSize())));
         view.renderMatrixOne(model.getMatrixOne());
     }
 
-    public void resetMatrixTwo() {
+    private void resetMatrixTwo() {
         model.setMatrixTwo(new Matrix(model.generator.generateRandomMatrix(model.getMatrixSize(),
                 model.getMatrixSize())));
         view.renderMatrixTwo(model.getMatrixTwo());
     }
 
-    public void resetIdentityMatrix() {
+    private void resetIdentityMatrix() {
         model.setIdentityMatrix(new Matrix(model.getMatrixOne().getIdentity()));
     }
 
-    public void resetDetLabels() {
+    private void resetDetLabels() {
         view.detALabel.setText("Determinant: ");
         view.detBLabel.setText("Determinant: ");
+    }
+
+    private void addMatrices() {
+        model.setMatrixThree(model.getMatrixOne().add(model.getMatrixOne(), model.getMatrixTwo()));
+        view.renderProductMatrix(model.getMatrixThree());
+    }
+
+    private void multiplyMatrices() {
+        model.setMatrixThree(model.getMatrixOne().multiply(model.getMatrixOne(), model.getMatrixTwo()));
+        view.renderProductMatrix(model.getMatrixThree());
+    }
+
+    private void downsizeMatrices() {
+        if(model.getMatrixSize() <= MATRIX_GRID_LOWER_LIMIT) {
+            System.out.println("Matrices cannot be decremented any further.");
+        } else {
+            model.setMatrixSize(model.getMatrixSize() - 1);
+            resetMatrixOne();
+            resetMatrixTwo();
+            resetIdentityMatrix();
+            resetDetLabels();
+            view.clearProductMatrix(); //if we change the size of our matrices then any pre-existing data in the
+            //product matrix should be cleared
+            System.out.println("Matrices decremented.");
+        }
+    }
+
+    private void upsizeMatrices() {
+        if(model.getMatrixSize() >= MATRIX_GRID_UPPER_LIMIT) {
+            System.out.println("Matrices cannot be incremented any further.");
+        } else {
+            model.setMatrixSize(model.getMatrixSize() + 1);
+            resetMatrixOne();
+            resetMatrixTwo();
+            resetIdentityMatrix();
+            resetDetLabels();
+            view.clearProductMatrix();
+            System.out.println("Matrices incremented.");
+        }
+    }
+
+    private void generateA() {
+        //call the generator's relevant method to return a new matrix
+        model.setMatrixOne(new Matrix(model.generator.generateRandomMatrix(model.getMatrixSize(),
+                model.getMatrixSize())));
+        //update the identity matrix in our model
+        model.setIdentityMatrix(new Matrix(model.getMatrixOne().getIdentity()));
+        //clear any pre-existing data in the product matrix and set the instance to null
+        model.setMatrixThree(null);
+        view.clearProductMatrix();
+        view.renderMatrixOne(model.getMatrixOne());
+        view.detALabel.setText("Determinant: ");
+    }
+
+    private void generateB() {
+        model.setMatrixTwo(new Matrix(model.generator.generateRandomMatrix(model.getMatrixSize(),
+                model.getMatrixSize())));
+        model.setIdentityMatrix(new Matrix(model.getMatrixTwo().getIdentity()));
+        model.setMatrixThree(null);
+        view.clearProductMatrix();
+        view.renderMatrixTwo(model.getMatrixTwo());
+        view.detBLabel.setText("Determinant: ");
+    }
+
+    private void viewIdentityA() {
+        if(!model.getMatrixOne().isIdentityInUse()) {
+            view.renderMatrixOne(model.getIdentityMatrix());
+            model.getMatrixOne().setIdentityInUse(true);
+            //after this, if the Matrix A getter method is called, the identity matrix will be returned
+        } else {
+            model.getMatrixOne().setIdentityInUse(false);
+            view.renderMatrixOne(model.getMatrixOne());
+            //after this, the Matrix A getter method will go back to returning the original matrix
+        }
+    }
+
+    private void viewIdentityB() {
+        if(!model.getMatrixTwo().isIdentityInUse()) {
+            view.renderMatrixTwo(model.getIdentityMatrix());
+            model.getMatrixTwo().setIdentityInUse(true);
+        } else {
+            model.getMatrixTwo().setIdentityInUse(false);
+            view.renderMatrixTwo(model.getMatrixTwo());
+        }
+    }
+
+    private void scaleA() {
+        model.setMatrixOne(model.getMatrixOne().scale(model.getMatrixOne(), model.getScaleFactorOne()));
+        view.renderMatrixOne(model.getMatrixOne());
+        model.setMatrixThree(null);
+        view.clearProductMatrix();
+        view.detALabel.setText("Determinant: ");
+        System.out.println("Matrix A scaled by a factor of: " + model.getScaleFactorOne());
+    }
+
+    private void scaleB() {
+        model.setMatrixTwo(model.getMatrixTwo().scale(model.getMatrixTwo(), model.getScaleFactorTwo()));
+        view.renderMatrixTwo(model.getMatrixTwo());
+        model.setMatrixThree(null);
+        view.clearProductMatrix();
+        view.detBLabel.setText("Determinant: ");
+        System.out.println("Matrix B scaled by a factor of: " + model.getScaleFactorTwo());
+    }
+
+    private void viewDeterminantA() {
+        model.getMatrixOne().setDeterminant(model.getMatrixOne());
+        view.detALabel.setText("Determinant: " + model.getMatrixOne().getDeterminant());
+        System.out.println("Matrix determinant is: " + model.getMatrixOne().getDeterminant());
+    }
+
+    private void viewDeterminantB() {
+        model.getMatrixTwo().setDeterminant(model.getMatrixTwo());
+        view.detBLabel.setText("Determinant: " + model.getMatrixTwo().getDeterminant());
+        System.out.println("Matrix determinant is: " + model.getMatrixTwo().getDeterminant());
     }
 }
